08b2b1f4d9d0ca7f159b2bdbd838736ce6a66a01
Abdurezak Farah <105218428+awelrisak@users.noreply.github.com>
2024-04-14 21:21:01 +0300

    Update infinite-moving-cards.tsx

--- src/components/ui/infinite-moving-cards.tsx
+++ src/components/ui/infinite-moving-cards.tsx
@@ -85,13 +85,16 @@ export const InfiniteMovingCards = ({
           pauseOnHover && "hover:[animation-play-state:paused]",
         )}
       >
-        {items.map((item, idx) => (
-          <li
-            className="relative w-[350px]  max-w-full flex-shrink-0 rounded-2xl border border-b-0 border-slate-700 px-8 py-6 md:w-[450px]"
-            style={{
+{/*
+style={{
               background:
                 "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
             }}
+*/}
+        {items.map((item, idx) => (
+          <li
+            className="bg-card text-card-foreground relative w-[350px]  max-w-full flex-shrink-0 rounded-2xl border border-b-0 border-slate-700 px-8 py-6 md:w-[450px]"
+            
             key={item.name}
           >
             <blockquote>
@@ -99,15 +102,15 @@ export const InfiniteMovingCards = ({
                 aria-hidden="true"
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
               ></div>
-              <span className=" relative z-20 text-sm font-normal leading-[1.6] text-gray-100">
+              <span className=" relative z-20 text-sm font-normal leading-[1.6] text-card-foreground">
                 {item.quote}
               </span>
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 <span className="flex flex-col gap-1">
-                  <span className=" text-sm font-normal leading-[1.6] text-gray-400">
+                  <span className=" text-sm font-normal leading-[1.6] text-muted-foreground">
                     {item.name}
                   </span>
-                  <span className=" text-sm font-normal leading-[1.6] text-gray-400">
+                  <span className=" text-sm font-normal leading-[1.6] text-muted-foreground">
                     {item.title}
                   </span>
                 </span>
