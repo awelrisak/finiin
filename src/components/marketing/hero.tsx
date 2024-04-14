5b2498fca94e21a0d0e5e5f9305e2a9a6eb772d3
Abdurezak Farah <105218428+awelrisak@users.noreply.github.com>
2024-04-15 00:06:33 +0300

    Update hero.tsx

--- src/components/marketing/hero.tsx
+++ src/components/marketing/hero.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
         fill="white"
       />
        
-      <h1 className="text-balance bg-gradient-to-b from-neutral-950 to-neutral-700 bg-clip-text px-6 pb-5 text-center font-heading text-4xl text-transparent dark:from-neutral-50 dark:to-neutral-400 md:text-7xl">
+      <h1 className="text-balance bg-gradient-to-b from-neutral-950 to-neutral-700 bg-clip-text px-6 pb-5 text-center font-heading text-4xl text-transparent dark:from-neutral-50 dark:to-red-400 md:text-7xl">
   Create, grow, and <br /> scale your business
 </h1>
 
