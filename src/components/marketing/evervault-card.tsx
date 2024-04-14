d783a746646ff804db3185a205d896c53bca45bf
Abdurezak Farah <105218428+awelrisak@users.noreply.github.com>
2024-04-14 21:29:23 +0300

    Update evervault-card.tsx

--- src/components/marketing/evervault-card.tsx
+++ src/components/marketing/evervault-card.tsx
@@ -3,7 +3,7 @@ import { EvervaultCard, Icon } from "@/components/ui/evervault-card";
 
 export function EvervaultCardSnippet() {
   return (
-    <div className="border border-white/[0.2] flex flex-col items-start max-w-sm mx-auto p-4 relative h-[30rem]">
+    <div className="border flex flex-col items-start max-w-sm mx-auto p-4 relative h-[30rem]">
       <Icon className="absolute h-6 w-6 -top-3 -left-3 text-white" />
       <Icon className="absolute h-6 w-6 -bottom-3 -left-3 text-white" />
       <Icon className="absolute h-6 w-6 -top-3 -right-3 text-white" />
@@ -11,11 +11,11 @@ export function EvervaultCardSnippet() {
 
       <EvervaultCard text="Sonamax" />
 
-      <h2 className="text-white  mt-4 text-sm font-light">
+      <h2 className="text-foreground  mt-4 text-sm font-light">
         Hover over this card to reveal an awesome effect. Running out of copy
         here.
       </h2>
-      <p className="text-sm border font-light border-white/[0.2]  rounded-full mt-4  text-white px-2 py-0.5">
+      <p className="text-sm border font-light border  rounded-full mt-4  text-foreground px-2 py-0.5">
         See designs
       </p>
     </div>
