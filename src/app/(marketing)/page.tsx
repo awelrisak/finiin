b5ae9602653f565ec18a5978203bec91d175dc09
Abdurezak Farah <105218428+awelrisak@users.noreply.github.com>
2024-04-14 19:51:40 +0300

    Style: Update page.tsx

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -7,17 +7,15 @@ import Services from "@/components/marketing/services";
 import ShopifyStores from "@/components/marketing/shopify-stores";
 import WebsiteDevelopment from "@/components/marketing/website-development";
 import SliderOne from "@/components/ui/slider";
-import StarsCanvas from "@/components/ui/srars";
+
 
 export default function Home() {
   return (
     <main className="relative z-10 mx-auto w-full p-4 px-2 pt-10 md:pt-20">
-      <div className="relative">
+      
         <Hero />
-        <StarsCanvas />
-      </div>
-
-      <div className="w-full pt-20">
+            
+      <div className="w-full pt-10">
         <SliderOne />
 
         <WebsiteDevelopment />
