790d48f924cc19757244769687e2f5216bf96fb1
awelrisak <abdurazak.awil1@gmail.com>
2024-04-16 17:58:03 +0300

    fix

--- src/components/marketing/pricing.tsx
+++ src/components/marketing/pricing.tsx
@@ -28,7 +28,7 @@ export default function Pricing({ title, text, prices }: PricingPops) {
         </div>
         <div className="space-y-8 sm:gap-6 lg:grid lg:grid-cols-3 lg:space-y-0 xl:gap-10">
           {prices.map((price) => (
-            <Price price={price} />
+            <Price price={price} key={price.title} />
           ))}
         </div>
       </div>
@@ -58,18 +58,7 @@ function Price({ price }: PriceProps) {
         <ul role="list" className="mb-8 space-y-4 text-left">
           {price.features.map((feature) => (
             <li className="flex items-center space-x-3">
-              {/* <svg
-              className="h-5 w-5 flex-shrink-0 text-green-500 dark:text-green-400"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                clip-rule="evenodd"
-              ></path>
-            </svg> */}
+             
               <Icons.check />
               <span>{feature}</span>
             </li>
