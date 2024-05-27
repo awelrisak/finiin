99a5f51c9024e03e8037b91722e191e96012e2de
Abdurezak Farah <105218428+awelrisak@users.noreply.github.com>
2024-05-27 23:54:13 +0300

    Update page.tsx

--- src/app/(marketing)/blog/page.tsx
+++ src/app/(marketing)/blog/page.tsx
@@ -52,7 +52,7 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
       {posts?.length ? (
         <div className="grid gap-10 sm:grid-cols-2 max-md:divide-y-2">
           {posts.map((post, index) => (
-            <Post post={post} key={post._id}/>
+            <Post post={post} key={post.slug}/>
           ))}
         </div>
       ) : (
