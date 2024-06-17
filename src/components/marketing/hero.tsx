cad7cb73e43ba062d3953bb002c49325ffc4909a
Abdurezak Farah <105218428+awelrisak@users.noreply.github.com>
2024-06-17 15:02:31 +0300

    Update hero.tsx

--- src/components/marketing/hero.tsx
+++ src/components/marketing/hero.tsx
@@ -7,21 +7,23 @@ export default function Hero() {
   return (
     <div className="relative py-6 min-h-[calc(100dvh-5rem)]">
       <Spotlight
-        className="-top-40 left-0 fill-white dark:hidden md:-top-20 md:left-60"
+        className="-top-40 left-0 fill-white md:-top-20 md:left-60"
         // fill="white"
       />
 
-      <h1 className="px-6 pb-5 md:text-center font-heading text-4xl text-balance bg-gradient-to-b from-neutral-950 to-neutral-500 bg-clip-text  text-transparent dark:from-neutral-50 dark:to-neutral-700 md:text-7xl">
+      <h1 className="px-6 pb-5 text-center font-heading text-4xl text-balance bg-gradient-to-b from-neutral-950 to-neutral-500 bg-clip-text  text-transparent dark:from-neutral-50 dark:to-neutral-700 md:text-7xl">
         More Customers. More Sales.
+        <br />
+        Finiin makes it possible.
       </h1>
 
-      <p className="mx-auto mb-6 mt-4 max-w-lg  px-4 md:text-center text-lg font-normal text-muted-foreground">
+      <p className="mx-auto mb-6 mt-4 max-w-lg  px-4 text-center text-lg font-normal text-muted-foreground">
           Whether you need a website, app, or marketing assistance, Finiin makes it possible. 
       </p>
 
       <div className="my-4" />
 
-      <div className="md:mx-auto my-6 flex w-48 items-center justify-center p-2">
+      <div className="mx-auto my-6 flex w-48 items-center justify-center p-2">
         <Button
           as={Link}
           href="book-a-call"
