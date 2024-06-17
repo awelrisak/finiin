c2f37fb253fe39a051134145dcb259bf4558b4d0
Abdurezak Farah <105218428+awelrisak@users.noreply.github.com>
2024-06-17 11:45:33 +0300

    Update hero.tsx

--- src/components/marketing/hero.tsx
+++ src/components/marketing/hero.tsx
@@ -7,15 +7,15 @@ export default function Hero() {
   return (
     <div className="relative py-6 min-h-[calc(100dvh-5rem)]">
       <Spotlight
-        className="-top-40 left-0 fill-white dark:fill-orange-900 md:-top-20 md:left-60"
+        className="-top-40 left-0 fill-white dark:hidden md:-top-20 md:left-60"
         // fill="white"
       />
 
-      <h1 className="text-balance bg-gradient-to-b from-neutral-950 to-neutral-500 bg-clip-text px-6 pb-5 text-center font-heading text-4xl text-transparent dark:from-neutral-50 dark:to-neutral-700 md:text-7xl">
+      <h1 className="px-6 pb-5 md:text-center font-heading text-4xl text-balance bg-gradient-to-b from-neutral-950 to-neutral-500 bg-clip-text  text-transparent dark:from-neutral-50 dark:to-neutral-700 md:text-7xl">
         More Customers. More Sales. <br/> Finiin Makes it Possible.
       </h1>
 
-      <p className="mx-auto mb-6 mt-4 max-w-lg  px-4 text-center text-lg font-normal text-muted-foreground">
+      <p className="mx-auto mb-6 mt-4 max-w-lg  px-4 md:text-center text-lg font-normal text-muted-foreground">
           Whether you need a website, app, or marketing assistance, we&apos;ve got you covered. 
       </p>
 
